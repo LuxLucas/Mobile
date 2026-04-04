38041e56f507828d4f906c85c9bb54bf320d6621
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2026-04-04 19:40:37 -0300

    :sparkles: feat(App): adicionar tela da calculadora ao App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { PaperProvider } from "react-native-paper";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { darkTheme, lightTheme } from "./styles/theme";
+import { useState } from "react";
+import Calculator from "./screens/Calculator";
+
 
 export default function App() {
+
+  const [isDark, setIsDark] = useState(false);
+  const toggleTheme = () => setIsDark(prev => !prev);
+
   return (
-    <>
-    </>
+    <SafeAreaProvider>
+      <PaperProvider theme={isDark ? darkTheme : lightTheme}>
+        <Calculator toggleTheme={toggleTheme} isDark={isDark} />
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
