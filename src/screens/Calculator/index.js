f218957d861bde02f2458acdb0b97e35b707a6a3
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2026-04-04 19:37:35 -0300

    :sparkles: feat: criar tela provisória da calculadora

---
+++ src/screens/Calculator/index.js
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+
+
+export default function Calculator ({
+    toggleTheme,
+    isDark,
+    children,
+    ...props
+}) {
+    return (
+        <View {...props} >
+            { children }
+        </View>
+    )
+}
