cb5a5c54c60e203c1b101832a87517365462d078
Lucas Flôres <lucaspizoniflores.github@gmail.com>
2026-04-18 17:50:27 -0300

    :recycle: refactor(Buffer): refatorar 'Buffer'

--- src/utils/Buffer.js
+++ src/utils/Buffer.js
@@ -1,67 +1,35 @@
-export default class Buffer{
-
-    #validDigits;
+export default class Buffer {
+    
     #value;
 
-    clearBuffer(){
-        this.#value = '0';
-    }
-    
-    constructor(){
-        this.#validDigits = [
-            '0', 
-            '1', 
-            '2', 
-            '3', 
-            '4', 
-            '5', 
-            '6', 
-            '7', 
-            '8', 
-            '9', 
-            '.'
-        ];
+    constructor() {
         this.clearBuffer();
     }
 
-    #lastDigit(){
-        return this.#value.slice(-1);
+    clearBuffer() {
+        this.#value = '0';
     }
 
-    getValue(){
-        if(this.#lastDigit() === '.') this.setValue('0');
-        return this.#value;
+    getValue() {
+        return this.#value.endsWith('.') 
+            ? this.#value.slice(0, -1) 
+            : this.#value;
     }
 
-    #digitIsValid(digit){
-        return digit in this.#validDigits;
-    }
+    addDigit(digit) {
+        if (!/[0-9]/.test(digit)) return;
 
-    #ifDigitIsValidThen(digit){
-        if(this.#value === '0' && digit !== '.'){
+        if (this.#value === '0') {
             this.#value = digit;
-            return
-        } 
-        this.#value += digit;
-    }
-
-    setValue(digit){
-        if(this.#digitIsValid(digit)){
-            this.#ifDigitIsValidThen(digit);
+        } else {
+            this.#value += digit;
         }
     }
 
-    addDigit(digit){
-        if(this.#value === '0' && digit === '0') return;
-        this.setValue(digit);
+    addPoint() {
+        if (!this.#value.includes('.')) {
+            this.#value += '.';
+        }
     }
-
-    #hasPoint(){
-        return this.#value.includes('.');
-    }
-
-    addPoint(){
-        if (!this.#hasPoint()) this.setValue('.');
-    }
-
+    
 }
